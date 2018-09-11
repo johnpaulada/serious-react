25af3f8346dc84ca26bff92cc7aa406cd225fe4a
John Paul <P10159@ISD-PF0XDGVM.gdcusaadc.com>
2018-09-11 18:47:44 +0800

    Resized Images

--- src/components/LandingHeader/index.js
+++ src/components/LandingHeader/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import BigContainer from "../BigContainer";
 import MainTitle from "../MainTitle";
 import MainSubtitle from "../MainSubtitle";
-import headerBackground from "./assets/landing-header-background.png";
+import headerBackground from "./assets/landing-header-background.jpg";
 
 class LandingHeader extends Component {
   render() {

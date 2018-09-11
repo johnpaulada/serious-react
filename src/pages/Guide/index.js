9231fb4751efe28499e49c5b0b5bb6e2a48db7cd
John Paul <P10159@ISD-PF0XDGVM.gdcusaadc.com>
2018-09-11 17:06:53 +0800

    Fixed Guide Animations

--- src/pages/Guide/index.js
+++ src/pages/Guide/index.js
@@ -1,27 +1,47 @@
 import React, { PureComponent } from "react";
-import { Parallax } from "react-spring";
+import { StyledParallax } from "./Styles";
 import Step from "./Step";
+
 import forestBackground from "./assets/forest.jpg";
 import snowBackground from "./assets/snow.jpg";
 
+const STEP_COUNT = 2;
+const steps = [
+  {
+    background: forestBackground,
+    title: "Prototype",
+    content: "Awesome content"
+  },
+  {
+    background: snowBackground,
+    title: "Storybook",
+    content: "Awesome content"
+  }
+];
+
 class Guide extends PureComponent {
-  scroll = to => this.refs.parallax.scrollTo(to);
+  setParallaxRef = element => {
+    this.parallax = element;
+  };
+  scroll = to => this.parallax.scrollTo(to);
   render() {
     return (
-      <Parallax ref="parallax" pages={2} horizontal scrolling={false}>
-        <Step
-          offset={0}
-          onClick={() => this.scroll(1)}
-          background={forestBackground}
-          title="Prototype"
-        />
-        <Step
-          offset={1}
-          onClick={() => this.scroll(0)}
-          background={snowBackground}
-          title="Storybook"
-        />
-      </Parallax>
+      <StyledParallax
+        innerRef={this.setParallaxRef}
+        pages={STEP_COUNT}
+        horizontal
+        scrolling={false}
+      >
+        {steps.map((step, index) => (
+          <Step
+            key={index}
+            offset={index}
+            onClick={() => this.scroll((index + 1) % STEP_COUNT)}
+            background={step.background}
+            title={step.title}
+          />
+        ))}
+      </StyledParallax>
     );
   }
 }
